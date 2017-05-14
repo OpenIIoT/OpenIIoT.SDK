1f1a33cccac269f1b7d39f2f440e623f919b2515
JP Dillingham <jpdillingham@gmail.com>
2017-05-14 13:16:16 -0500

    Added stubbed projects

---
+++ OpenIIoT.SDK.Packaging/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenIIoT.SDK.Packaging
+{
+    public class Class1
+    {
+    }
+}
